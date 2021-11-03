1b1f8f12ce74f977aba3b587146e75ab1cd68722
Alexandra <le.onze.mai@gmail.com>
2021-11-03 22:16:54 +0300

    theme changing and counter done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-let apple = 1;
+let body = document.querySelector('.js-body');
+let themeButton = document.querySelector('.js-theme-button');
+let countButton = document.querySelector('.js-count-button');
+let pressNumber = document.querySelector('.count-circle');
+let counter = 0;
+let display = document.querySelector('.js-display');
+
+/* Меняем тему */
+themeButton.onclick = function () {
+  body.classList.toggle('pink-theme');
+}
+
+/* Счетчик нажатий */
+countButton.onclick = function () {
+  counter++;
+  pressNumber.textContent = counter;
+  countButton.classList.toggle('pressed');
+}
