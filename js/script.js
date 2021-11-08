7eb730af7556f745205a58b5288a50489dcc3a30
Alexandra <le.onze.mai@gmail.com>
2021-11-08 23:46:26 +0300

    Typing of digits in display and clear - done

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ let countButton = document.querySelector('.js-count-button');
 let pressNumber = document.querySelector('.count-circle');
 let counter = 0;
 let display = document.querySelector('.js-display');
+let digitButtons = document.querySelectorAll('.js-button-digit');
+let clearButton = document.querySelector('.js-clear-button');
 
 /* Меняем тему */
 themeButton.onclick = function () {
@@ -15,4 +17,16 @@ countButton.onclick = function () {
   counter++;
   pressNumber.textContent = counter;
   countButton.classList.toggle('pressed');
+}
+
+/* Вывод цифр */
+
+for (let digitButton of digitButtons) {
+  digitButton.onclick = function () {
+    display.value = display.value + digitButton.value;
+  }
+}
+
+clearButton.onclick = function () {
+  display.value = 0;
 }
