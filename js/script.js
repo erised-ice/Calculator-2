452c185a15d8c9ec7f2243fae3bd32f8eab24e62
Alexandra <le.onze.mai@gmail.com>
2021-11-11 11:07:34 +0300

    Add different operations

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@ let pressNumber = document.querySelector('.count-circle');
 let counter = 0;
 let display = document.querySelector('.js-display');
 let digitButtons = document.querySelectorAll('.js-button-digit');
+let operationButtons = document.querySelectorAll('.js-button-operations');
 let clearButton = document.querySelector('.js-clear-button');
+let resultButton = document.querySelector('.js-result-button');
 
 /* Меняем тему */
 themeButton.onclick = function () {
@@ -19,47 +21,54 @@ countButton.onclick = function () {
   countButton.classList.toggle('pressed');
 }
 
-/* Вывод цифр */
+/* Калькулятор */
 
-/*for (let digitButton of digitButtons) {
-  digitButton.onclick = function (event) {
+/* storage */
+let operation = '';
+let firstOperand = '';
+let secondOperand = '';
+/**/
+
+for (let operationButton of operationButtons) {
+  operationButton.onclick = function (event) {
     event.preventDefault();
-    display.value = display.value + digitButton.value;
+    operation = operationButton.value;
   }
 }
 
-clearButton.onclick = function () {
-  display.value = 0;
-}
-*/
-
-/* Сумма */
-
-/* storage */
-let operation = null;
-let firstOperand = '';
-let secondOperand = '';
-let result = '';
-/**/
-let sumButton = document.querySelector('.js-button-sum');
-
-sumButton.onclick = function (event) {
-  event.preventDefault();
-  operation = sumButton.value;
-}
-
-resultButton = document.querySelector('.result');
 resultButton.onclick = function (event) {
   event.preventDefault();
-  result = 'result';
-  display.value = firstOperand + secondOperand;
+  firstOperand = Number(firstOperand);
+  secondOperand = Number(secondOperand);
+  
+  if (operation === 'sum') {
+    display.value = firstOperand + secondOperand;
+  } else if (operation === 'minus') {
+    display.value = firstOperand - secondOperand;
+  } else if (operation === 'divide') {
+    display.value = firstOperand / secondOperand;
+  } else if (operation === 'multiply') {
+    display.value = firstOperand * secondOperand;
+  }
+  
+  firstOperand = '';
+  secondOperand = '';
+  operation = '';
+}
+
+clearButton.onclick = function (event) {
+  event.preventDefault();
+  display.value = '';
+  firstOperand = '';
+  secondOperand = '';
+  operation = '';
 }
 
 for (let digitButton of digitButtons) {
   digitButton.onclick = function (event) {
     event.preventDefault();
     
-    if (operation === 'sum') {
+    if (operation !== '') {
       secondOperand = secondOperand + digitButton.value;
       display.value = secondOperand;
     } else {
